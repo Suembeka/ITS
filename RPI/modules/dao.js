e80d1c938a82e565f4ed0ddb6955411b031e384d
Sourcebite <sourcebite@gmail.com>
2017-08-26 22:46:01 +0500

    Update dao methods

--- RPI/modules/dao.js
+++ RPI/modules/dao.js
@@ -1,36 +1,73 @@
-var mysql = require('mysql');
+var MySQL = require('mysql');
+var Logger = require('logger');
 
-var dbModule = {
-    db: {},
+var DAO = {
+    connection: null,
+
     isInit: false,
-    state: {},
-    init: function () {
-        db = mysql.createConnection({
-            host: 'localhost',
-            port: '/var/run/mysqld/mysqld.sock',
-            user: 'rpibus',
-            password: '12345678qwerty',
-            database: 'bus_system'
-        });
-	console.log('Connection to db created');
 
-        db.connect(function (err) {
-            if (err){
-			logger.writeLog('db.txt', err);
-		}
-            isInit = true;
-	    console.log('Init = true');
-            db.query('(SELECT transport_id FROM transport) UNION (SELECT payment_amount FROM misc)', function (err, result) {
-                if (err) {
-			logger.writeLog('db.txt', err);
-		}
-		console.log(result.transportID);
-		console.log(result.paymentAmount);
-		state.transportID = result.transportID;
-                state.paymentAmount = result.paymentAmount;
-            });
+    connectionOptions: {
+        host: 'localhost',
+        port: '/var/run/mysqld/mysqld.sock',
+        user: 'rpibus',
+        password: '12345678qwerty',
+        database: 'bus_system'
+    },
+
+    connect: function() {
+        DAO.connection = MySQL.createConnection(connectionOptions);
+        
+        DAO.connection.connect(function(err) {
+            if(DAO.logError(err)) {
+                isInit = false;
+            } else {
+                isInit = true;
+                
+            }
         });
     },
+
+    logError: function(err) {
+        if(err) {
+            logger.log({
+                file: __filename,
+                dir: __dirname,
+                error: err
+            });
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    // TODO: move to controller
+    db: {},
+    init: function() {
+        DAO.connect();
+        db = connection;
+        DAO.getTransportID();
+        DAO.getPaymentAmount();
+    },
+
+    state: {},
     curStation : 1,
-    curStationOrder : 1
+    curStationOrder : 1,
+
+    getTransportID: function() {
+        DAO.connection.query('SELECT transport_id FROM transport', function(err, result) {
+            if(!DAO.logError(err)) {
+                DAO.state.transportID = result.transportID;
+            }
+        });
+    },
+
+    getPaymentAmount: function() {
+        DAO.connection.query('SELECT payment_amount FROM misc', function(err, result) {
+            if(!DAO.logError(err)) {
+                DAO.state.paymentAmount = result.paymentAmount;
+            }
+        });
+    }
 };
+
+module.exports = DAO;
