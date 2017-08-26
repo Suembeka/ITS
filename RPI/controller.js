77eb90a65eab2a5be3db21cf5ab8d1e5a627757c
Суембека Ахмаджанова < >
2017-08-26 13:36:35 +0500

    Added node_modules

--- RPI/controller.js
+++ RPI/controller.js
@@ -1,11 +1,21 @@
 // Приклепленные файлы
-var logger = require('./modules/logger.js');
-var gps = require('./modules/gps.js');
-var dao = require('./modules/dao.js');
-var net_controller = require('./modules/net_controller.js');
-var notifier = require('./modules/notifier.js');
-var parser = require('./modules/parser.js');
-var usb_controller = require('./modules/usb_controller.js');
+var logger_file = require('./modules/logger.js');
+var gps_file = require('./modules/gps.js');
+var dao_file = require('./modules/dao.js');
+var net_controller_file = require('./modules/net_controller.js');
+var notifier_file = require('./modules/notifier.js');
+var parser_file = require('./modules/parser.js');
+var usb_controller_file = require('./modules/usb_controller.js');
+
+var SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline;
+const parser = new Readline();
+
+var GPS = require('gps');
+var gps = new GPS;
+
+const portGPS = new SerialPort('/dev/ttyS0');
+portGPS.pipe(parser);
 
 var UUID = 0;
 
@@ -15,7 +25,11 @@ var count = 1;
 const radius = 3;
 //var buffer = [];
 
-
-/*port.on('close', function () {
-    connection.end();
-});*/
+parser.on('data', function (data) {
+	gps.update(data);
+}).on('error', function (err) {
+        logger.writeLog('gps.txt', err);
+});
+gps.on('data', function () {
+	gps_file.readGpsData();
+});
--- RPI/modules/gps.js
+++ RPI/modules/gps.js
@@ -1,66 +1,49 @@
 var GPS = require('gps');
-var gps = new GPS;
 
-var SerialPort = require('serialport');
-var fileGPS = '/dev/ttyS0';
+/*parser.on('data', console.log);*/
+/*port.write('ROBOT PLEASE RESPOND\n');*/
 
-const Readline = SerialPort.parsers.Readline;
-const port = new SerialPort(fileGPS);
-const parser = new Readline();
-port.pipe(parser);
-	/*parser.on('data', console.log);*/
-	/*port.write('ROBOT PLEASE RESPOND\n');*/
+var gpsObject = {
+    readGpsData: function () {
+        /*if (!dbModule.isInit) {
+            return;
+        }*/
+	
+	var data = gps.state;
 
-gps.on('data', function(){
-	/*console.log(gps.state);
-	console.log('gps.onRMC run');*/
-	GPSprocessing();
-});
+        if (!data.lat || !data.lon) {
+            return;
+        }
 
-parser.on('data', function (data) {
-    gps.update(data);
+        console.log(data.lat, data.lon);
 
-}).on('error', function (err) {
-    logger.writeLog('gps.txt', err);
-});
+        /*dbModule.db.query('SELECT * FROM routes_stations;', function (err, result) {
+		if (err) {
+				logger.writeLog('db.txt', err);
+			}
+		
+		result.filter(function (station) {
+		    var latlng = station.latlng.split(',');
+		    var dist = GPS.Distance(latlng[0], latlng[1], data.lat, data.lon) * 1000;
+		
+		    if (dist <= radius) {
+		        if (station.stationOrder === (dbModule.state.curStationOrder + 1)) {
+		            cur_station_id = dbModule.state.curStationOrder + 1;
+		
+		            dbModule.query("UPDATE misc SET station_id = " + station.id + ";", function (err) {
+		                if (err) {
+								logger.writeLog('db.txt', err);
+							}
+		                logger.writeLog('db.txt', 'Current station has changed');
+		            });
+		        } else {
+		
+		        }
+		    }
+		});
+    		});*/
+	}
+};
 
-function GPSprocessing() {
-    if (!dbModule.isInit) {
-        return;
-    }
 
-    var data = gps.state;
-
-
-    if (!data.lat || !data.lon) {
-        return;
-    }
-
-    console.log(data.lat, data.lon);
-
-    /*dbModule.db.query('SELECT * FROM routes_stations;', function (err, result) {
-        if (err) {
-			logger.writeLog('db.txt', err);
-		}
-
-        result.filter(function (station) {
-            var latlng = station.latlng.split(',');
-            var dist = GPS.Distance(latlng[0], latlng[1], data.lat, data.lon) * 1000;
-
-            if (dist <= radius) {
-                if (station.stationOrder === (dbModule.state.curStationOrder + 1)) {
-                    cur_station_id = dbModule.state.curStationOrder + 1;
-
-                    dbModule.query("UPDATE misc SET station_id = " + station.id + ";", function (err) {
-                        if (err) {
-							logger.writeLog('db.txt', err);
-						}
-                        logger.writeLog('db.txt', 'Current station has changed');
-                    });
-                } else {
-
-                }
-            }
-        });
-    });*/
-}
+module.exports = gpsObject;
