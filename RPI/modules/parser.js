88611120605280987525123e7870f74d91402e5e
Sourcebite <sourcebite@gmail.com>
2017-08-27 17:25:29 +0500

    Add arduino parser template

---
+++ RPI/modules/parser.js
@@ -0,0 +1,25 @@
+var EventEmitter = require('events');
+
+var Parser = {};
+
+class ArduinoParser extends EventEmitter {
+    tokens = {
+        commandStart: '[',
+        commandEnd: ']',
+        commandSequenceStart: '[S]',
+        commandSequenceEnd: '[E]',
+        commandWrite: '[W(PIN{2})(DATA{16})]',
+        responseRead: '[(PIN{2})(DATA{16})]',
+        responseStatusOK: '[STATUS|OK]',
+        responseStatusFail: '[STATUS|FAIL]'
+    }
+
+    processData = function(data) {
+        
+    }
+};
+
+Parser.arduino = new ArduinoParser();
+
+
+modules.exports = Parser;
