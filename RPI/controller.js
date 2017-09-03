cf460835ad000e83601285eb527cb46303ec53fc
Suembeka Akhmadjanova <suembeka@gmail.com>
2017-09-03 10:00:31 +0500

    Change initDB function

--- RPI/controller.js
+++ RPI/controller.js
@@ -6,53 +6,68 @@ const GPS = require('./modules/gps.js');
 const Arduino = require('./modules/arduino.js');
 
 class App {
-	getCurrentStation(){ return this.currentStation; }
-	getTransportID(){ return this.transportID; }
-	getPaymentAmount(){ return this.paymentAmount; }
+    getCurrentStation() {
+        return this.currentStation;
+    }
+    getTransportID() {
+        return this.transportID;
+    }
+    getPaymentAmount() {
+        return this.paymentAmount;
+    }
 
-	setCurrentStation(val){ currentStation = val; }
-	setTransportID(val){ transportID = val; }
-	setPaymentAmount(val){ paymentAmount = val; }
+    setCurrentStation(val) {
+        currentStation = val;
+    }
+    setTransportID(val) {
+        transportID = val;
+    }
+    setPaymentAmount(val) {
+        paymentAmount = val;
+    }
 
-	initDB() {
-		DAO.connect();
-	}
+    initDB() {
+        DAO.init();
+    }
 
-	initState() {
-		// const appInitData = DAO.getAppInitData();
-		// setCurrentStation(appInitData.currentStation);
-		// setTransportID(appInitData.transportID);
-		// setPaymentAmount(appInitData.paymentAmount);
-	}
+    initState() {
+        // const appInitData = DAO.getAppInitData();
+        // setCurrentStation(appInitData.currentStation);
+        // setTransportID(appInitData.transportID);
+        // setPaymentAmount(appInitData.paymentAmount);
+    }
 
-	initGPS() {
-    setInterval(function () {
-        GPS.processGPS(DAO);
-    }, 1000);
-	}
+    initGPS() {
+        setInterval(function () {
+            GPS.processGPS(DAO);
+        }, 1000);
+    }
 
-	initArduino() {
-		Arduino.on('cardFound', this.processPayment);
-		Arduino.on('writeStatus', this.acceptTranscation);
-	}
+    initArduino() {
+        Arduino.on('cardFound', this.processPayment);
+        Arduino.on('writeStatus', this.acceptTranscation);
+    }
 
-	processPayment(card, arduinoID) {
-		console.log(card);
-		if(Date.now() - card.lastPaytime < 5000) { console.log('Reject'); return; }
+    processPayment(card, arduinoID) {
+        console.log(card);
+        if (Date.now() - card.lastPaytime < 5000) {
+            console.log('Reject');
+            return;
+        }
 
-		card = {
-			cardType: 1,
-			balance: 5000,
-			expireTime: 1506077646044,
-			lastTransportID: 123,
-			lastPaytime: Date.now()
-		};
-		Arduino.write(card, arduinoID);
-	}
+        card = {
+            cardType: 1,
+            balance: 5000,
+            expireTime: 1506077646044,
+            lastTransportID: 123,
+            lastPaytime: Date.now()
+        };
+        Arduino.write(card, arduinoID);
+    }
 
-	acceptTranscation(paymentStatus, arduinoID) {
-		//paymentStatus = true or false
-	}
+    acceptTranscation(paymentStatus, arduinoID) {
+        //paymentStatus = true or false
+    }
 };
 
 const app = new App();
@@ -62,5 +77,5 @@ app.initGPS();
 app.initArduino();
 
 process.on('unhandledRejection', (reason, p) => {
-	console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
 });
