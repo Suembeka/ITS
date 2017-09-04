11a22e3c53a857372c3a46661834c4cb36fa1af1
Suembeka Akhmadjanova <suembeka@gmail.com>
2017-09-04 23:08:15 +0500

    Beautify

--- RPI/modules/parser.js
+++ RPI/modules/parser.js
@@ -16,30 +16,31 @@ class Parser extends EventEmitter {
         //console.log('PARSER: ', data);
 
         var block;
+
         data = data.replace(/[\r\n]/g, '');
 
-        if(/\[([0-9]{2})(.{32})]/.test(data)) {
+        if (/\[([0-9]{2})(.{32})]/.test(data)) {
             block = data.match(/^\[([0-9]{2})(.{32})]$/);
             cardBlocks[block[1]] = block[2];
-        } else if(/\[E\]/.test(data)) {
+        } else if (/\[E\]/.test(data)) {
             this.emit('cardFound', cardBlocks, arduinoID);
             cardBlocks = {};
-        } else if(/\[STATUS\|OK\]/.test(data)) {
+        } else if (/\[STATUS\|OK\]/.test(data)) {
             this.emit('writeStatus', true, arduinoID);
-        } else if(/\[STATUS\|FAIL\]/.test(data)) {
+        } else if (/\[STATUS\|FAIL\]/.test(data)) {
             this.emit('writeStatus', false, arduinoID);
         }
     }
 
-	encode(blocks) {
+    encode(blocks) {
         var newLine = '\r\n';
         var msg = '[S]' + newLine;
-        for(var blockNum in blocks) {
+        for (var blockNum in blocks) {
             msg += '[W' + blockNum + blocks[blockNum] + ']' + newLine;
         }
         msg += '[E]' + newLine;
         return msg;
-	}
+    }
 };
 
 Parser = new Parser();
